a6e9c9a4331fc0030251910dcf4d90e3fa6581dd
zhang_anzhi <zhang_anzhi@126.com>
2021-04-26 18:51:29 +0800

    refactor(ChatBridge): add classes instance in ChatBridge

--- src/main/java/io/github/zhanganzhi/chatbridge/ChatBridge.java
+++ src/main/java/io/github/zhanganzhi/chatbridge/ChatBridge.java
@@ -1,34 +1,53 @@
 package io.github.zhanganzhi.chatbridge;
 
+import io.github.zhanganzhi.chatbridge.manager.BroadcastManager;
 import net.md_5.bungee.api.plugin.Plugin;
-import io.github.zhanganzhi.chatbridge.event.Event;
-import io.github.zhanganzhi.chatbridge.manager.ServerNameManager;
+import io.github.zhanganzhi.chatbridge.event.EventListener;
+import io.github.zhanganzhi.chatbridge.manager.ConfigManager;
+import io.github.zhanganzhi.chatbridge.manager.MessageManager;
 import io.github.zhanganzhi.chatbridge.advancedban.AdvancedBan;
 
 public final class ChatBridge extends Plugin {
-    private ServerNameManager serverNameManager;
+    private BroadcastManager broadcastManager;
+    private ConfigManager configManager;
+    private MessageManager messageManager;
     private boolean loadAdvancedBan = false;
     private AdvancedBan advancedBan;
 
     @Override
     public void onEnable() {
-        serverNameManager = new ServerNameManager(this);
+        configManager = new ConfigManager(this);
+        messageManager = new MessageManager(this);
+        broadcastManager = new BroadcastManager(this);
         if (getProxy().getPluginManager().getPlugin("AdvancedBan") != null) {
             this.loadAdvancedBan = true;
             this.advancedBan = new AdvancedBan();
         }
-        getProxy().getPluginManager().registerListener(this, new Event(this));
+        getProxy().getPluginManager().registerListener(this, new EventListener(this));
     }
 
-    public ServerNameManager getServerMapManager() {
-        return this.serverNameManager;
+    @Override
+    public void onDisable() {
+        broadcastManager.stopServer();
     }
 
-    public boolean getLoadAdvancedBan() {
-        return this.loadAdvancedBan;
+    public BroadcastManager getBroadcastManager() {
+        return broadcastManager;
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public MessageManager getMessageManager() {
+        return messageManager;
+    }
+
+    public boolean isLoadAdvancedBan() {
+        return loadAdvancedBan;
     }
 
     public AdvancedBan getAdvancedBan() {
-        return this.advancedBan;
+        return advancedBan;
     }
 }
